60c2e8fba3e06d79531f7a47571745a8a12c4f3f
Anthony Moreira <anthony.moreira@webbfontaine.com>
2017-07-20 11:40:55 +0200

    Adding fix for development mode

--- src/main/groovy/asset/pipeline/i18n/I18nProcessor.groovy
+++ src/main/groovy/asset/pipeline/i18n/I18nProcessor.groovy
@@ -22,16 +22,21 @@ package asset.pipeline.i18n
 import asset.pipeline.AbstractProcessor
 import asset.pipeline.AssetCompiler
 import asset.pipeline.AssetFile
+import grails.io.IOUtils
+import grails.util.Holders
 import groovy.transform.CompileStatic
+import org.grails.plugins.BinaryGrailsPlugin
+import org.springframework.core.io.DefaultResourceLoader
 import org.springframework.core.io.FileSystemResourceLoader
+import org.springframework.core.io.Resource
+import org.springframework.core.io.ResourceLoader
 import org.springframework.core.io.support.ResourcePatternResolver
+
+
 import org.springframework.core.io.support.ResourcePatternUtils
 
 import java.util.regex.Matcher
-import org.springframework.core.io.DefaultResourceLoader
-import org.springframework.core.io.Resource
-import org.springframework.core.io.ResourceLoader
-
+import java.util.regex.Pattern
 
 /**
  * The class {@code I18nProcessor} represents an asset processor which converts
@@ -43,7 +48,7 @@ import org.springframework.core.io.ResourceLoader
  * <ul>
  *   <li>The file name (without extension) must end with the locale
  *   specification, e. g. {@code messages_de.i18n} or
- * {@code msg_en_UK.i18n}.</li>
+ *   {@code msg_en_UK.i18n}.</li>
  *   <li>The files are line based.</li>
  *   <li>All lines are trimmed (that is, whitespaces are removed from beginning
  *   and end of lines.</li>
@@ -51,13 +56,13 @@ import org.springframework.core.io.ResourceLoader
  *   are ignored.</li>
  *   <li>Lines starting with <code>@import <i>path</i></code> are replaced by
  *   the content of the file with path <code><i>path</i></code>.  The suffix
- * {@code .i18n} at path is optional and is appended automatically.</li>
+ *   {@code .i18n} at path is optional and is appended automatically.</li>
  *   <li>All other lines are treated as code keys which will be looked up in
  *   Grails message resources for the locale specified in the file.</li>
  * </ul>
  *
- * @author Daniel Ellermann
- * @author David Estes
+ * @author  Daniel Ellermann
+ * @author  David Estes
  * @version 3.0
  */
 @CompileStatic
@@ -68,9 +73,11 @@ class I18nProcessor extends AbstractProcessor {
     protected static final String PROPERTIES_SUFFIX = '.properties'
     protected static final String XML_SUFFIX = '.xml'
 
+
     //-- Fields ---------------------------------
 
     ResourceLoader resourceLoader = new DefaultResourceLoader()
+
 
     //-- Constructors ---------------------------
 
@@ -78,11 +85,12 @@ class I18nProcessor extends AbstractProcessor {
      * Creates a new i18n resource processor within the given asset
      * pre-compiler.
      *
-     * @param precompiler the given asset pre-compiler
+     * @param precompiler   the given asset pre-compiler
      */
     I18nProcessor(AssetCompiler precompiler) {
         super(precompiler)
     }
+
 
     //-- Public methods -------------------------
 
@@ -90,12 +98,14 @@ class I18nProcessor extends AbstractProcessor {
     String process(String inputText, AssetFile assetFile) {
         Matcher m = assetFile.name =~ /._(\w+)\.i18n$/
         StringBuilder buf = new StringBuilder('messages')
-        if (m) buf << '_' << m.group(1)
-        Properties props = loadMessages(buf.toString())
-        println('-------Properties content-----------')
-        props.list(System.out)
-
-        println("Input text : $inputText")
+        String locale = m ? m.group(1) : null
+        if (locale) buf << '_' << locale
+        Properties props
+        if (assetFile.encoding != null) {
+            props = loadMessages(buf.toString(), locale, assetFile.encoding)
+        } else {
+            props = loadMessages(buf.toString(), locale)
+        }
         // At this point, inputText has been pre-processed (I18nPreprocessor).
         Map<String, String> messages = [:]
         inputText.toString()
@@ -104,7 +114,6 @@ class I18nProcessor extends AbstractProcessor {
                 messages.put line, props.getProperty(line, line)
             }
         }
-        println('Messages for JS : '+messages.toString())
         compileJavaScript messages
     }
 
@@ -113,13 +122,13 @@ class I18nProcessor extends AbstractProcessor {
     /**
      * Compiles JavaScript code from the given localized messages.
      *
-     * @param messages the given messages
-     * @return the compiled JavaScript code
+     * @param messages  the given messages
+     * @return          the compiled JavaScript code
      */
     private String compileJavaScript(Map<String, String> messages) {
-        StringBuilder buf = new StringBuilder('''(function (win) {
-    var messages = {
-''')
+        Pattern pattern = Pattern.compile(/\{(\d{1,2})}/)
+        Pattern split = Pattern.compile(/((?<=(\{\d{1,2}}))|(?=(\{\d{1,2}})))/)
+        StringBuilder buf = new StringBuilder()
         int i = 0
         for (Map.Entry<String, String> entry in messages.entrySet()) {
             if (i++ > 0) {
@@ -129,65 +138,135 @@ class I18nProcessor extends AbstractProcessor {
                     .replace('\\', '\\\\')
                     .replace('\n', '\\n')
                     .replace('"', '\\"')
-            buf << '        "' << entry.key << '": "' << value << '"'
+            if (value.find(pattern)) {
+                List<String> x = split.split(value).collect { String part ->
+                    Matcher matcher = pattern.matcher(part)
+                    if (matcher.find()) {
+                        return matcher.group(1)
+                    } else {
+                        return '"' + part + '"'
+                    }
+                }
+                buf << '        "' << entry.key << '": [' << x.join(", ") << ']'
+            } else {
+                buf << '        "' << entry.key << '": "' << value << '"'
+            }
         }
-        buf << '''
+        return getJavaScriptCode(buf.toString())
     }
 
-    win.$L = function (code) {
-        return messages[code];
-    }
-}(this));
-'''
+    /**
+     * Return the javascript function that will create $L
+     * The function can use parameters to pass to messages
+     * Eg :  $L('code', ['foo':'bar', 'bar':'foo'] )
+     * or $L('code', 21)
+     * @param messages the javascript object containing the messages in a key value pair structure
+     * @return String containing the complete javascript function
+     */
+    String getJavaScriptCode(String messages) {
+        StringBuilder buf = new StringBuilder('''(function (win) {
+            if (win.i18n_messages) {
+                var tmpMsg = {
+                                ''')
+                    buf << messages
+                    buf << '''
+                }
+                for (var attrname in tmpMsg) { win.i18n_messages[attrname] = tmpMsg[attrname]; }
+            }
+            else {
+                win.i18n_messages = {
+                '''
+                buf << messages
+                buf << '''
+                    };
+            }
+            var messages = win.i18n_messages;
+            win.$L = function (code) {
+                var message = messages[code];
+                if(message === undefined) {
+                    return "[" + code + "]";
+                } else if(message instanceof Array) {
+                    var params;
+                    if (arguments.length === 2 && (arguments[1]) instanceof Array) {
+                        params = arguments[1];
+                    } else {
+                        params = Array.prototype.slice.call(arguments);
+                        params.shift();
+                    }
+                    var result = "";
+                    for(var i = 0; i < message.length; i++) {
+                        if(typeof message[i] === "number") {
+                            result += params[message[i]];
+                        } else {
+                            result += message[i];
+                        }
+                    }
+                    return result;
+                } else {
+                    return message;
+                }
+            }
+        }(this));
+        '''
+
         buf.toString()
     }
 
     /**
      * Loads the message resources from the given file.
      *
-     * @param fileName the given base file name
-     * @return the read message resources
+     * @param fileName                  the given base file name
+     * @param locale                    the locale of the properties file
+     * @param                           encoding the charset of the properties file
+     * @return                          the read message resources
      * @throws FileNotFoundException    if no resource with the required
      *                                  localized messages exists
      */
-    private Properties loadMessages(String fileName) {
+    private Properties loadMessages(String fileName, String locale, String encoding = 'utf-8') {
         List<Resource> listRes = locateResource(fileName)
-        Properties props = null
+        Properties props = new Properties()
         for (resource in listRes) {
-            if (!props) {
-                props = new Properties()
+            String propertiesString = IOUtils.toString(resource.inputStream, encoding)
+            props.load(new StringReader(propertiesString))
+        }
+        if (Holders.pluginManager?.allPlugins != null) {
+            for (plugin in Holders.pluginManager.allPlugins) {
+                if (plugin instanceof BinaryGrailsPlugin) {
+                    Locale loc = locale ? new Locale(locale) : new Locale('en')
+                    Properties propPlugin = ((BinaryGrailsPlugin) plugin).getProperties(loc)
+                    if (propPlugin != null) {
+                        props.putAll(propPlugin)
+                    }
+                }
             }
-            else {
-                props = new Properties(props)
-            }
-            props.load(resource.inputStream)
+        }
+        if (props.isEmpty()) {
+            throw new FileNotFoundException('File '+fileName+' has not been found')
         }
         props
     }
 
     /**
-     * Locates the resources containing the localized messages.  The method looks
+     * Locates the resource containing the localized messages.  The method looks
      * in the following places:
      * <ul>
      *   <li>in classpath with extension {@code .properties}</li>
      *   <li>in classpath with extension {@code .xml}</li>
      *   <li>in file system in folder {@code grails-app/i18n} with extension
-     * {@code .properties}</li>
+     *   {@code .properties}</li>
      *   <li>in file system in folder {@code grails-app/i18n} with extension
-     * {@code .xml}</li>
-     *  <li>in a list of jar supplied in comma separated string in the com.i18n-asset-pipeline.pluginRuntimePath
-     *  system env property<li>
+     *   {@code .xml}</li>
      * </ul>
      *
-     * @param fileName the given base file name
-     * @return the resource containing the messages
+     * @param fileName                  the given base file name
+     * @return                          the resource containing the messages
      * @throws FileNotFoundException    if no resource with the required
      *                                  localized messages exists
      */
     private List<Resource> locateResource(String fileName) {
         List<Resource> resourceList = []
         Resource resource =
-                resourceLoader.getResource("classpath*:"+fileName + PROPERTIES_SUFFIX)
+                resourceLoader.getResource("classpath*:" + fileName + PROPERTIES_SUFFIX)
         if (resource.exists()) {
             resourceList << resource
         }
@@ -220,7 +299,9 @@ class I18nProcessor extends AbstractProcessor {
                 for (res in found) {
                     if (res.exists()) {
                         resourceList.add(res)
-                        println('Found for path ' + path + ' : ' + found.size())
+
+
+                        println("Found file ${fileName}${PROPERTIES_SUFFIX} for path $path : ${found.size()}")
                     }
                 }
             }
