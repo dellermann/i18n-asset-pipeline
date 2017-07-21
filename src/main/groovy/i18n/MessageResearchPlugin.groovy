3b1da34a0b18f7ca4910f5dc2046beedf46f5701
Anthony Moreira <anthony.moreira@webbfontaine.com>
2017-07-21 16:27:01 +0200

    Fixing typo and removing unecessary code

--- src/main/groovy/i18n/MessageResearchPlugin.groovy
+++ src/main/groovy/i18n/MessageResearchPlugin.groovy
@@ -11,20 +11,12 @@ class MessageResearchPlugin implements Plugin<Project>{
                 target.configurations.runtime.files.each { f->
                     builder << f.toString()+','
                 }
-                new File('classpath_runtime.txt').withWriter('UTF-8') { w ->
-                    w.write(builder.toString())
-                }
-                println('Runtime classpath executed : '+builder.toString())
                 System.setProperty("com.i18n-asset-pipeline.pluginRuntimePath", builder.toString())
             }
         }
         target.afterEvaluate {
             def assetTask = target.getTasksByName('assetCompile',true)
             assetTask.each {
-                it.dependsOn << 'getRuntimeClasspath'
-            }
-            def bootrunTask = target.getTasksByName('bootRun',true)
-            bootrunTask.each {
                 it.dependsOn << 'getRuntimeClasspath'
             }
         }
